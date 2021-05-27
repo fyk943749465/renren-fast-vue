4f1a06a32cfdfea2ecfa75f84674b4c6abd9a19d
fyk943749465 <fyk943749465@gmail.com>
2021-05-26 20:11:35 -0700

    product edit add delete udpate

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,5 +1,48 @@
 <template>
-<el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+<div>
+  <el-tree :data="data" :props="defaultProps" show-checkbox node-key="catId" :expand-on-click-node="false" :default-expanded-keys="expandedKey">
+    <span class="custom-tree-node" slot-scope="{ node, data }">
+        <span>{{ node.label }}</span>
+        <span>
+          <el-button v-if="data.childCategoryList.length != 0"
+            type="text"
+            size="mini"
+            @click="() => append(data)">
+            Append
+          </el-button>
+          <el-button v-else
+            type="text"
+            size="mini"
+            @click="() => remove(node, data)">
+            Delete
+          </el-button>
+          <el-button
+            type="text"
+            size="mini"
+            @click="() => edit(node, data)">
+            Edit
+          </el-button>
+        </span>
+      </span>
+  </el-tree>
+  <el-dialog :title="title" :visible.sync="dialogVisible" width="30%">
+    <el-form :model="category">
+      <el-form-item label="Category Name">
+        <el-input v-model="category.name" auto-complete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="Icon">
+        <el-input v-model="category.icon" auto-complete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="Product Unit">
+        <el-input v-model="category.productUnit" auto-complete="off"></el-input>
+      </el-form-item>
+    </el-form>
+    <span slot="footer" class="dialog-footer">
+      <el-button @click="dialogVisible = false">Cancel</el-button>
+      <el-button type="primary" @click="submitData">Confirm</el-button>
+    </span>
+  </el-dialog>
+</div>
 </template>
 
 <script>
@@ -8,58 +51,121 @@ export default {
   props: {},
   data() {
     return {
-      data: [
-        {
-          label: 'Level one 1',
-          children: [{
-            label: 'Level two 1-1',
-            children: [{
-              label: 'Level three 1-1-1'
-            }]
-          }]
-        }, {
-          label: 'Level one 2',
-          children: [{
-            label: 'Level two 2-1',
-            children: [{
-              label: 'Level three 2-1-1'
-            }]
-          }, {
-            label: 'Level two 2-2',
-            children: [{
-              label: 'Level three 2-2-1'
-            }]
-          }]
-        }, {
-          label: 'Level one 3',
-          children: [{
-            label: 'Level two 3-1',
-            children: [{
-              label: 'Level three 3-1-1'
-            }]
-          }, {
-            label: 'Level two 3-2',
-            children: [{
-              label: 'Level three 3-2-1'
-            }]
-          }]
-        }
-      ],
+      title: '',
+      dialogType: '', // edit, add
+      category: {name: '', parentCid: 0, catLevel: 0, showStatus: 1, sort: 0, icon: '', productUnit: '', catId: null},
+      dialogVisible: false,
+      data: [],
+      expandedKey: [],
       defaultProps: {
-        children: 'children',
-        label: 'label'
+        children: 'childCategoryList',
+        label: 'name'
       }
     }
   },
   methods: {
-    handleNodeClick(data) {
-      console.log(data)
-    },
     getMenus() {
       this.$http({
         url: this.$http.adornUrl('/product/category/list/tree'),
         method: 'get'
-      }).then(data => console.log('get mueu data.....', data))
+      }).then(({data}) => {
+        this.data = data.data
+      })
+    },
+    append(data) {
+      console.log('append', data)
+      this.dialogType = 'add'
+      this.title = 'Add Category'
+      this.dialogVisible = true
+      this.category.parentCid = data.catId
+      this.category.catLevel = data.catLevel * 1 + 1
+    },
+    submitData() {
+      if (this.dialogType === 'add') {
+        this.addCategory()
+      }
+      if (this.dialogType === 'edit') {
+        this.editCategory()
+      }
+    },
+    addCategory() {
+      console.log('add data:', this.category)
+      this.$http({
+        url: this.$http.adornUrl('/product/category/save'),
+        method: 'post',
+        data: this.$http.adornData(this.category, false)
+      }).then(({data}) => {
+        this.$message({
+          type: 'success',
+          message: 'Save success'
+        })
+        this.dialogVisible = false
+        this.getMenus()
+        this.expandedKey = [this.category.parentCid]
+      })
+    },
+    editCategory() {
+      var {catId, name, icon, productUnit} = this.category
+      this.$http({
+        url: this.$http.adornUrl('/product/category/update'),
+        method: 'post',
+        data: this.$http.adornData({catId, name, icon, productUnit}, false)
+      }).then(({data}) => {
+        this.$message({
+          message: 'menu edit success',
+          type: 'success'
+        })
+        this.dialogVisible = false
+        this.getMenus()
+        this.expandedKey = [this.category.parentCid]
+      })
+    },
+    remove(node, data) {
+      var ids = [data.catId]
+      this.$confirm(`This ${data.name} will permanently delete the file. Continue?`, 'Warning', {
+        confirmButtonText: 'OK',
+        cancelButtonText: 'Cancel',
+        type: 'warning'
+      }).then(() => {
+        this.$http({
+          url: this.$http.adornUrl('/product/category/delete'),
+          method: 'post',
+          data: this.$http.adornData(ids, false)
+        }).then(({data}) => {
+          this.$message({
+            type: 'success',
+            message: 'Delete completed'
+          })
+          this.getMenus()
+          this.expandedKey = [node.parent.data.catId]
+        })
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: 'Delete canceled'
+        })
+      })
+    },
+    edit(node, data) {
+      this.title = 'Edit Category'
+      this.dialogType = 'edit'
+      this.dialogVisible = true
+      console.log('edit', node, data)
+      // you must send request and get the back data,
+      // because not only one use the system and the data has been modified, so the data we see now it's fault
+      this.$http({
+        url: this.$http.adornUrl(`/product/category/info/${data.catId}`),
+        method: 'get'
+      }).then(({ data }) => {
+        console.log('servcer back data', data)
+        this.category.name = data.category.name
+        this.category.catId = data.category.catId
+        this.category.icon = data.category.icon
+        this.category.productUnit = data.category.productUnit
+        this.category.parentCid = data.category.parentCid
+      })
+      // this.category.name = data.name
+      // this.category.catId = data.catId
     }
   },
   computed: {},
