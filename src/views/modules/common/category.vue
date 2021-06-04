1e899746cd1d4c5a6c3201d571b605fdf116c8d5
fyk943749465 <fyk943749465@gmail.com>
2021-06-03 19:50:28 -0700

    属性分组, 父子传值 cascader

---
+++ src/views/modules/common/category.vue
@@ -0,0 +1,50 @@
+<template>
+    <el-tree :data="data" :props="defaultProps" @node-click="nodeclick"></el-tree>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {
+      data: [],
+      defaultProps: {
+        children: 'childCategoryList',
+        label: 'name'
+      }
+    }
+  },
+  computed: {},
+  watch: {},
+  methods: {
+    getMenus() {
+      this.$http({
+        url: this.$http.adornUrl('/product/category/list/tree'),
+        method: 'get'
+      }).then(({data}) => {
+        this.data = data.data
+      })
+    },
+    nodeclick(data, node, component) {
+      console.log('category click:', data, node, component)
+      // send event to parent
+      this.$emit('tree-node-click', data, node, component)
+    }
+  },
+  created() {
+    this.getMenus()
+  },
+  beforeCreate() {},
+  beforeMount() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeDestory() {},
+  destoryed() {},
+  activated() {}
+}
+</script>
+
+<style scoped>
+
+</style>
